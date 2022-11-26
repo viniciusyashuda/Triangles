8e6bfa5d01b281c0ace2c8747e6cb95ff1e0f2e4
shuda <viniciusyashuda008@gmail.com>
2022-11-25 22:36:22 -0300

    ajustando lógica de lado 0 e melhorando a estrutura

--- Triangles/Program.cs
+++ Triangles/Program.cs
@@ -8,36 +8,31 @@ namespace Triangles
         {
             float side1, side2, side3;
 
-            Console.WriteLine("Enter the first side of the triangle:");
+            Console.Write("Enter the triangle's first side measure:");
             side1 = float.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the second side of the triangle:");
+
+            Console.Write("Enter the triangle's second side measure:");
             side2 = float.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the third side of the triangle:");
+
+            Console.Write("Enter the triangle's third side measure:");
             side3 = float.Parse(Console.ReadLine());
 
-            if ((side1 < 0) || (side2 < 0) || (side3 < 0))
-            {
-                Console.WriteLine("The value of a side can not be a negative number");
-            }
+            if (side1 <= 0 || side2 <= 0 || side3 <= 0)
+                Console.WriteLine("The value of a side can not be a negative number or zero");
+
             else
             {
-                if ((side1 == side2) & (side2 == side3))
-                {
+                if (side1 == side2 && side2 == side3)
                     Console.WriteLine("The triangle is equilateral");
-                }
+
+                else if (side1 != side2 && side1 != side3 && side2 != side3)
+                    Console.WriteLine("The triangle is scalene");
+
                 else
-                {
-                    if ((side1 != side2) & (side1 != side3) & (side2 != side3))
-                    {
-                        Console.WriteLine("The triangle is scalene");
-                    }
-                    else
-                    {
-                        Console.WriteLine("The triangle is isosceles");
-                    }
-                }
-                  
+                    Console.WriteLine("The triangle is isosceles");
+
             }
+
             Console.ReadKey();
         }
     }
